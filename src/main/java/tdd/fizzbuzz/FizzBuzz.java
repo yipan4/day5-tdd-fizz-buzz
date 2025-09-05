8a98b2a9d757dd738d186dfed26489708da3d026
Andy Yip <andy.yip@oocl.com>
2025-09-05 11:44:26 +0800

    Feat: implemented case for count is multiple of 3 and 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -7,6 +7,9 @@ public class FizzBuzz {
     public static String countFizzBuzz(int count) {
         boolean isMultipleOf3 = count % 3 == 0;
         boolean isMultipleOf5 = count % 5 == 0;
+        if (isMultipleOf3 && isMultipleOf5) {
+            return FIZZBUZZ;
+        }
         if (isMultipleOf3 && !isMultipleOf5) {
             return FIZZ;
         }
