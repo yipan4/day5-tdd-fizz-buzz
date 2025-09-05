65cc75091880660ade74dfb1da3d4b463f7da3c8
Andy Yip <andy.yip@oocl.com>
2025-09-05 11:37:15 +0800

    Feat: implemented the case of not being multiple of 3 or 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,11 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public static String countFizzBuzz(int count) {
+        boolean isMultipleOf3 = count % 3 == 0;
+        boolean isMultipleOf5 = count % 5 == 0;
+        if (!isMultipleOf3 && !isMultipleOf5) {
+            return String.valueOf(count);
+        }
         return null;
     }
 }
