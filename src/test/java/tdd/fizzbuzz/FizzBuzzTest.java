5dfdce641ed886cbf4fa0ed9b448dd3f2831578d
Andy Yip <andy.yip@oocl.com>
2025-09-05 11:39:26 +0800

    Test: added test case for count is multiple of 3'

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -5,6 +5,9 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+    private static final String FIZZBUZZ = "Fizzbuzz";
     @Test
     public void should_return_number_when_count_given_not_multiple_of_3_or_5() {
         // Given
@@ -13,5 +16,15 @@ public class FizzBuzzTest {
         String result = FizzBuzz.countFizzBuzz(count);
         // Expect
         assertEquals("1",result);
+    }
+
+    @Test
+    public void should_return_fizz_when_count_given_is_multiple_of_3_only() {
+        // Given
+        int count = 3;
+        // When
+        String result = FizzBuzz.countFizzBuzz(count);
+        // Expect
+        assertEquals(FIZZ, result);
     }
 }
