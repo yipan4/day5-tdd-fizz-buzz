bd61d23f670af158116623b0c843cbf3c5f2b6c5
Andy Yip <andy.yip@oocl.com>
2025-09-05 11:41:14 +0800

    Feat: added implementation for count is multiple of 3 only

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,9 +1,15 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+    private static final String FIZZBUZZ = "Fizzbuzz";
     public static String countFizzBuzz(int count) {
         boolean isMultipleOf3 = count % 3 == 0;
         boolean isMultipleOf5 = count % 5 == 0;
+        if (isMultipleOf3 && !isMultipleOf5) {
+            return FIZZ;
+        }
         if (!isMultipleOf3 && !isMultipleOf5) {
             return String.valueOf(count);
         }
